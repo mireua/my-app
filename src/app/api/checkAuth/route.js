08eb0f2340b2e55921af0055ccea7473931dfcf4
mireu (Samuel) <103215918+mireua@users.noreply.github.com>
2023-12-18 21:44:54 +0000

    0.3, fix with auth

--- src/app/api/checkAuth/route.js
+++ src/app/api/checkAuth/route.js
@@ -1,11 +1,14 @@
-import { cookies } from 'next/headers'
+import { cookies } from 'next/headers';
+
 export async function GET(req, res) {
-// Make a note we are on
-// the api. This goes to the console.
-console.log("checking auth")
-// get the auth record
-let record = cookies().get('auth');
-console.log(record.value);
-// at the end of the process we need to send something back.
-return Response.json({ "status":"" + record.value + ""})
+  // Make a note we are on the api. This goes to the console.
+  console.log("checking auth");
+
+  // Get the auth record or set it to 'false' if it's undefined
+  let record = cookies().get('auth') || { value: 'false' };
+
+  console.log(record.value);
+
+  // At the end of the process, send something back.
+  return Response.json({ "status": "" + record.value + "" });
 }
